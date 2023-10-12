76731190b3682ed57fcbebc95b6b0f92e64e92fe
chamchop <ocfmiller@protonmail.com>
2023-10-12 18:38:39 +0100

    chaning homepage and adding routes

---
+++ frontend/src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>HOME</h1>
+        <p className="text-muted">home</p>
+      </div>
+    </div>
+  );
+}
