7ba0a10f7dfe71d347cde1122fcd8964b52c63ed
chamchop <ocfmiller@protonmail.com>
2023-10-12 18:58:26 +0100

    login page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "./containers/Login.tsx";
 import Home from "./containers/Home.tsx";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
